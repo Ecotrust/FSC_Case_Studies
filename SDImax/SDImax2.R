857f7752d92ca5a729cc49c028d54b4f1f95a89b
sloreno <sara.loreno@gmail.com>
2017-07-20 13:19:28 -0700

    SDImax updates
    Fixes #3

--- SDImax/SDImax2.R
+++ SDImax/SDImax2.R
@@ -30,6 +30,24 @@ groupColumns = c("Variant","Stratum_1_Species_1")
 
 #calculate summary stats: 98%, 95%, and 90%, count of records in group
 stats<-ddply(compile, groupColumns, summarise, Max = max(SDI), SDI98 = quantile(SDI, .98), SDI95 = quantile(SDI, .95),
-             SDI90 = quantile(SDI, .90),  freq=length(Variant))
+             SDI90 = quantile(SDI, .90),  SDI85=round(SDI95*.85), freq=length(Variant))
 
-write.csv(stats, file="SDIstats.csv")
+#subset data to only species with 100+ counts and remove -- species
+stats100<-subset(stats, stats$freq>=100 & stats$Stratum_1_Species_1!="--")
+
+#create variant specific tables
+WC_stats<-subset(stats100, stats100$Variant=="WC")
+PN_stats<-subset(stats100, stats100$Variant=="PN")
+
+#function to write variant KCP SDImax files
+f <- function(x, output) {
+  species <- x[2]
+  SDI85 <- x[7]
+  print(paste("SDIMax           ", species, "       ", SDI85, "                           55.        75", sep=""))
+  cat(paste("SDIMax           ", species, "       ", SDI85, "                           55.        75", sep=""), 
+      file= output, append = T, fill = T)
+}
+
+#write SDImax KCP files
+apply(WC_stats, 1, f, output = 'SDImax_WC.KCP')
+apply(PN_stats, 1, f, output = 'SDImax_PN.KCP')
