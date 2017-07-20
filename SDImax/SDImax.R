8e26f5abd457795c364803fa32c33539df835029
sloreno <sara.loreno@gmail.com>
2017-07-20 09:23:01 -0700

    Upload both input options for R

--- SDImax/SDImax.R
+++ SDImax/SDImax.R
@@ -6,30 +6,25 @@ library(sqldf)
 library(tcltk2)
 library(gdata)
 library(plyr)
+library(RODBC)
 
-#read in FVS_Cases table
-cases = read.csv("FVS_Cases.csv")  
+con <- odbcConnectAccess2007("G:/projects/FSC_CaseStudies_2016/Data/Work/AllAreas/SDImax/FVS_SDImax_out.accdb")
 
-#read in FVS_Compute table
-compute = read.csv("FVS_Compute.csv")
-
-#read in FVS_StrClass table
-structure = read.csv("FVS_StrClass.csv") 
-
-#read in FVS_Summary table
-summary = read.csv("FVS_Summary.csv") 
+#pull in Cost Benefit Tables
+compute<-data.frame(sqlFetch(con, "FVS_Compute"))
+structure<-data.frame(sqlFetch(con, "FVS_StrClass"))
+summary<-data.frame(sqlFetch(con, "FVS_Summary"))
+cases<-data.frame(sqlFetch(con, "FVS_Cases"))
 
 compile<-sqldf("SELECT compute.Year, compute.PERBA1, structure.Stratum_1_Species_1, summary.SDI, cases.Variant
                 FROM ((compute INNER JOIN structure ON (compute.Year = structure.Year) AND (compute.StandID = structure.StandID)) 
-                INNER JOIN summary ON (structure.Year = summary.Year) AND (structure.StandID = summary.StandID)) 
+                INNER JOIN summary ON (structure.Year = FVS_Summary.Year) AND (structure.StandID = summary.StandID)) 
                 INNER JOIN cases ON compute.CaseID = cases.CaseID
                 WHERE (((compute.Year)=2014) AND ((compute.PERBA1)>0.8));")
 
-#group by variant and species type
-groupColumns = c("Variant","Stratum_1_Species_1")
 
 #calculate summary stats: 98%, 95%, and 90%, count of records in group
-stats<-ddply(compile, groupColumns, summarise, Max = max(SDI), SDI98 = quantile(SDI, .98), SDI95 = quantile(SDI, .95),
+stats<-ddply(stands_SDI, groupColumns, summarise, SDI98 = quantile(SDI, .98), SDI95 = quantile(SDI, .95),
              SDI90 = quantile(SDI, .90),  freq=length(Variant))
 
 write.csv(stats, file="SDIstats.csv")
