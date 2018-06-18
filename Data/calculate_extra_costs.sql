2d55289cde7f6cba74d93827eec2c35af1c64f1e
David Diaz <ddiaz@ecotrust.org>
2018-06-18 13:09:49 -0700

    Updated SQL script for calculating costs, commented out ADD COLUMN queries

--- Data/calculate_extra_costs.sql
+++ Data/calculate_extra_costs.sql
@@ -1,13 +1,5 @@
--- SET npv_total, nfv_total, and cumulative_MBF to zero for pretend harvests
-UPDATE fvs_econsummary
-SET npv_total = 0,
-	nfv_total = 0,
-	cumulative_MBF = 0
-WHERE fvs_econsummary.pretend_harvest = 'YES';
-
-
-ALTER TABLE fvs_econsummary
-ADD COLUMN cumulative_MBF real;
+--ALTER TABLE fvs_econsummary
+--ADD COLUMN cumulative_MBF real;
 
 -- calculate cumulative harvest volume
 UPDATE fvs_econsummary
@@ -57,8 +49,10 @@ UPDATE fvs_compute
 		ORDER BY caseid, year) foo
 	WHERE fvs_compute.caseid = foo.caseid AND fvs_compute.year = foo.year;
 
-ALTER TABLE fvs_econsummary
-ADD COLUMN total_undiscounted_cost real;
+
+--ALTER TABLE fvs_econsummary
+--ADD COLUMN total_undiscounted_cost real;
+
 -- calculate ECON costs + extra computed costs, using undiscounted future value
 UPDATE fvs_econsummary
 SET total_undiscounted_cost = extra.total_cost_undisc
@@ -67,8 +61,10 @@ SET total_undiscounted_cost = extra.total_cost_undisc
 			WHERE fvs_compute.caseid = fvs_econsummary.caseid AND fvs_compute.year = fvs_econsummary.year) extra
 	WHERE extra.caseid = fvs_econsummary.caseid AND extra.year = fvs_econsummary.year;
 
-ALTER TABLE fvs_econsummary
-ADD COLUMN total_discounted cost real;
+
+--ALTER TABLE fvs_econsummary
+--ADD COLUMN total_discounted cost real;
+
 -- calculate ECON costs + extra computed costs, using present value
 UPDATE fvs_econsummary
 SET total_discounted_cost = extra.total_cost_disc
@@ -79,8 +75,9 @@ SET total_discounted_cost = extra.total_cost_disc
 
 
 
-ALTER TABLE fvs_econsummary
-ADD COLUMN npv_total real;
+--ALTER TABLE fvs_econsummary
+--ADD COLUMN npv_total real;
+
 -- calculate total NPV
 UPDATE fvs_econsummary
 SET npv_total = discounted_revenue - total_discounted_cost;
@@ -90,3 +87,10 @@ ADD COLUMN nfv_total real;
 -- calculate total net future value, undiscounted
 UPDATE fvs_econsummary
 SET nfv_total = undiscounted_revenue - total_undiscounted_cost;
+
+-- SET npv_total, nfv_total, and cumulative_MBF to zero for pretend harvests
+UPDATE fvs_econsummary
+SET npv_total = 0,
+	nfv_total = 0,
+	cumulative_MBF = 0
+WHERE fvs_econsummary.pretend_harvest = 'YES';
